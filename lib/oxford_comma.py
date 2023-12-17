979ac14605f50af9c7412b8da0c19351e76f567f
Daniel Morley <morleydan10@gmail.com>
2023-12-17 02:05:27 -0500

    passes first 2 tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,13 @@
 def oxford_comma(items):
-    return None
+
+    # new_list = ", ".join(items[:-1]) + ", and " + items[-1]
+
+    # print(new_list)
+
+
+    return " and ".join(items)
+
+
+
+# 
+# ", ".join(items[:-1]) + ", and " + items[-1]
